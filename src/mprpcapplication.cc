6379e19fe5fb27d407fa68deed6956c653f213ec
workWsl <weicunzhe@qq.com>
2024-10-18 17:43:34 +0800

    解析argc

--- src/mprpcapplication.cc
+++ src/mprpcapplication.cc
@@ -1,6 +1,43 @@
 #include "mprpcapplication.h"
+#include <iostream>
+#include <unistd.h>
 
-void MprpcApplication::Init(int argc, char **argv) {
+void ShowArgsHelp()
+{
+    std::cout << "format: command -i <configfile>" << std::endl;
+}
+
+void MprpcApplication::Init(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        ShowArgsHelp();
+        exit(EXIT_FAILURE);
+    }
+
+    int c = 0;
+    std::string config_file;
+    while ((c = getopt(argc, argv, "i:")) != -1)
+    {
+        switch (c)
+        {
+        case 'i':
+            config_file = optarg;
+            break;
+        case '?':
+            std::cout << "invalid args!" << std::endl;
+            ShowArgsHelp();
+            exit(EXIT_FAILURE);
+        case ':':
+            std::cout << "need <configfile>!" << std::endl;
+            ShowArgsHelp();
+            exit(EXIT_FAILURE);
+        default:
+            break;
+        }
+    }
+
+    // 开始加载配置文件 rpc_server_ip= rpc_server_port= zookeeper_ip= zookeeper_port=
 
 };
 MprpcApplication &MprpcApplication::GetInstatnce()
